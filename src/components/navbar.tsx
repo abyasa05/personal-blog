f423b925298dc4c9f5d9a747655a5a01c998406d
abyasa05 <abyprat07@gmail.com>
2025-07-17 13:01:33 +0700

    Add base navbar & color constants

---
+++ src/components/navbar.tsx
@@ -0,0 +1,10 @@
+export default function Navbar() {
+    return (
+        <nav className="flex items-center justify-between">
+            <div>Abyasa's Website</div>
+            <div className="flex items-center gap-8">
+                <a href=''>Home</a>
+            </div>
+        </nav>
+    )
+}
---
+++ src/elements/color.ts
@@ -0,0 +1,6 @@
+export const colors = {
+    primary: "#00BF99",
+    secondary: "#00B0D8",
+    baseLight: "#FFFFFF",
+    baseDark: "#1E1E1E",
+}
