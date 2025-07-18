e07f2d59f2c5665dced781bc36fadd996a8c901c
abyasa05 <abyprat07@gmail.com>
2025-07-18 18:55:23 +0700

    Configure fonts & global CSS

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,9 +1,11 @@
+import Link from "next/link"
+
 export default function Navbar() {
     return (
         <nav className="flex items-center justify-between">
             <div>Abyasa's Website</div>
             <div className="flex items-center gap-8">
-                <a href=''>Home</a>
+                <Link href=''>Home</Link>
             </div>
         </nav>
     )
