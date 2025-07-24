866fd5d78b1cc442a2cf73850721505a9ff2d866
abyasa05 <abyprat07@gmail.com>
2025-07-24 21:49:18 +0700

    Completed footer links & icons

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -10,8 +10,24 @@ export default function Footer() {
             <div className="row-span-1 col-span-1 flex px-8 items-center justify-end font-semibold" style={{ backgroundColor: colors.secondary, color: colors.baseLight }}>
                 Built Using
             </div>
-            <div className="row-span-1 col-span-1" style={{ backgroundColor: colors.primary }}></div>
-            <div className="row-span-1 col-span-1" style={{ backgroundColor: colors.primary }}></div>
+            <div className="row-span-1 col-span-1 flex items-center px-8 gap-3 text-4xl" style={{ backgroundColor: colors.primary, color: colors.baseLight }}>
+                <Link href="https://www.instagram.com/m.aby.07" target="_blank" className="socialLinks">
+                    <i className="fa-brands fa-instagram"></i>
+                </Link>
+                <Link href="https://x.com/MabyasaP" target="_blank" className="socialLinks">
+                    <i className="fa-brands fa-x-twitter"></i>
+                </Link>
+                <Link href="https://www.linkedin.com/in/muhammad-abyasa-pratama-86956328a" target="_blank" className="socialLinks">
+                    <i className="fa-brands fa-linkedin"></i>
+                </Link>
+                <Link href="https://github.com/abyasa05" target="_blank" className="socialLinks">
+                    <i className="fa-brands fa-github"></i>
+                </Link>
+            </div>
+            <div className="row-span-1 col-span-1 flex items-center gap-8 px-8 justify-end" style={{ backgroundColor: colors.primary }}>
+                    <div className="bg-[url('/logo/next_wordmark.svg')] bg-right bg-no-repeat bg-contain h-[2rem] w-1/2"></div>
+                    <div className="bg-[url('/logo/tailwind-wordmark.svg')] bg-right bg-no-repeat bg-contain h-[2rem] w-1/2"></div>
+            </div>
             <div className="row-span-1 col-span-2 flex px-8 items-center justify-center" style={{ backgroundColor: colors.baseDark, color: colors.baseLight }}>
                 © 2025. All rights reserved.
             </div>
