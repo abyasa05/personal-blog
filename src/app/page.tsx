0fe257c12b4fe8acdb762c7700a9fb6b9154eb46
abyasa05 <abyprat07@gmail.com>
2025-08-03 22:27:16 +0700

    More adjustments for titles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,17 +16,24 @@ export default function Home() {
           height={785} 
           className="w-full h-auto min-h-[785px] object-[80%_100%] object-cover"
         />
-        <span className="title font-extrabold text-[8rem] md:text-[12rem]" style={{ color: colors.baseLight }}>
-          <span className="absolute top-95 -left-3.5">Hello</span>
-          <span className="absolute top-142.5 left-15">There</span>
-        </span>
+        {/* <span className="title font-extrabold text-[8rem] sm:text-[12rem]" style={{ color: colors.baseLight }}>
+          <p className="absolute top-95 -left-3.5">Hello</p>
+          <p className="absolute top-142.5 left-15">There</p>
+        </span> */}
+        <div className="absolute flex flex-col top-[70%] sm:top-[55%] -left-2.5 sm:-left-3.5 title font-extrabold text-[8rem] sm:text-[12rem] leading-none" style={{ color: colors.baseLight }}>
+          <p>Hello</p>
+          <div className="flex">
+            <div className="h-auto w-[3rem] sm:w-[5rem]"></div>
+            <p>There</p>
+          </div>
+        </div>
       </div>
-      <div className="content text-[1rem] md:text-[1.1rem] sidePadding pt-20 pb-16 text-justify max-w-full">
+      <div className="content text-[1rem] sm:text-[1.1rem] sidePadding py-12 sm:py-20 text-justify max-w-full">
         My name is Muhammad Abyasa Pratama, people used to call me Aby (or Abyasa). I was born on 22 July 2005 in Jakarta, Indonesia. I'm currently enrolling as an undergraduate computer science student at the University of Indonesia. I'm a hard working and curious person who wishes to excel at my field of interests.
       </div>
       <div className="sectionWrapper">
-        <div className="content text-[1rem] sidePadding py-10 text-justify relative border-t-24 min-w-full" style={{ backgroundColor: colors.primary, color: colors.baseLight }}>
-          <div className="absolute title title-2 font-bold pl-4 pb-[0.5rem] -top-24" style={{ color: colors.primary }}>Interest</div>
+        <div className="content text-[1rem] sidePadding py-10 text-justify relative" style={{ backgroundColor: colors.primary, color: colors.baseLight }}>
+          <div className="absolute title title-2 font-bold pl-5 sm:pl-4 -top-13 sm:-top-24" style={{ color: colors.primary }}>Interest</div>
           <p>
             As of now, I'm mainly into the field of front-end development and UI/UX design, but this is subjected to change. Let me tell you, I used to be a person who has quite a fascination with design and aesthetics. Now, since I've decided to dive into the world of IT, I want to use this interest to get more understanding about what builds up a good user interface design. I know that the field of UI/UX design is way more complicated than just about aesthetics, but I'm sure interested to master every part of it. 
           </p>
@@ -36,12 +43,12 @@ export default function Home() {
         </div>
       </div>
       <div className="sectionWrapper">
-        <div className="relative titleBox ml-12 md:ml-15.5" style={{ backgroundColor: colors.primary, color: colors.baseLight }}>
-          <span className="absolute title title-2 font-bold -left-2.5 md:-left-4.5 top-[0.2rem] md:-top-0.5">Education</span>
+        <div className="relative titleBox ml-12 sm:ml-15.5" style={{ backgroundColor: colors.primary, color: colors.baseLight }}>
+          <span className="absolute title title-2 font-bold -left-2.5 sm:-left-4.5 top-[0.2rem] sm:-top-0.5">Education</span>
         </div>
-        <div className="flex flex-col md:flex-row gap-10 sidePadding pt-10 justify-center">
+        <div className="flex flex-col sm:flex-row gap-10 sidePadding pt-10 justify-center">
           <div className="flex flex-col gap-8 items-center">
-            <div className="h-40 md:h-50 w-auto">
+            <div className="h-40 sm:h-50 w-auto">
               <Image 
                 src='/logo/dupan.png'
                 alt='Logo of SMAN 28 Jakarta'
@@ -55,13 +62,13 @@ export default function Home() {
               <span style={{ opacity: 0.7 }}>2020 - 2023</span>
             </div>
           </div>
-          <div id="connectLine" className="flex flex-col md:flex-row gap-0 md:h-50 items-center">
+          <div id="connectLine" className="flex flex-col sm:flex-row gap-0 sm:h-50 items-center">
             <div className="w-2 h-2 rounded-full" style={{ backgroundColor: colors.primary }}></div>
-            <div className="w-0.5 h-12 md:w-12 md:h-0.5" style={{ backgroundColor: colors.primary }}></div>
+            <div className="w-0.5 h-12 sm:w-12 sm:h-0.5" style={{ backgroundColor: colors.primary }}></div>
             <div className="w-2 h-2 rounded-full" style={{ backgroundColor: colors.primary }}></div>
           </div>
           <div className="flex flex-col gap-8 items-center">
-            <div className="h-40 md:h-50 w-auto">
+            <div className="h-40 sm:h-50 w-auto">
               <Image 
                 src='/logo/makaraUI.png'
                 alt='Logo of University of Indonesia'
@@ -78,10 +85,10 @@ export default function Home() {
         </div>
       </div>
       <div className="sectionWrapper">
-        <div className="relative flex titleBox w-[18.5rem] md:w-[32rem]" style={{ backgroundColor: colors.primary, color: colors.baseLight }}>
-          <span className= "absolute title title-2 font-bold -top-2 md:-top-5">Proficiency</span>
+        <div className="relative flex titleBox w-[18.5rem] sm:w-[32rem]" style={{ backgroundColor: colors.primary, color: colors.baseLight }}>
+          <span className= "absolute title title-2 font-bold -top-2 sm:-top-5">Proficiency</span>
         </div>
-        <div className="scrollWrapper flex gap-6 md:gap-10 py-12 sidePadding overflow-x-auto">
+        <div className="scrollWrapper flex gap-6 md:gap-10 py-12 px-6 md:px-10 overflow-x-auto">
           <TechCard logo="python.svg" name="Python" color={{ borderColor: colors.primary }}/>
           <TechCard logo="java.svg" name="Java" color={{ borderColor: colors.primary }}/>
           <TechCard logo="html.svg" name="HTML" color={{ borderColor: colors.primary }}/>
--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -3,7 +3,7 @@ import { colors } from "@/elements/color"
 
 export default function Navbar() {
     return (
-        <nav className="flex items-center justify-between sticky top-0 z-50 title text-xl px-10 py-[1.5rem]" style={{ backgroundColor: colors.baseLight, color: colors.primary }}>
+        <nav className="flex items-center justify-between sticky top-0 z-50 title text-xl sidePadding py-[1.5rem]" style={{ backgroundColor: colors.baseLight, color: colors.primary }}>
             <div className="font-bold">Abyasa's Website</div>
         </nav>
     )
